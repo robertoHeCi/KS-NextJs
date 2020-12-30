2bceac9cd2b07cbf33c5f529bc77ffeac0d1025e
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2020-12-30 19:48:39 +0100

    feat: retrieve posts in client side

--- src/pages/data-fetching/single-page-application.js
+++ src/pages/data-fetching/single-page-application.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import getConfig from 'next/config'
+
+// Only holds serverRuntimeConfig and publicRuntimeConfig
+const { publicRuntimeConfig } = getConfig()
 
 const Spa = () => {
-  return (<div>spa</div>)
+  const [posts, setPosts] = React.useState([])
+  React.useEffect(() => {
+    fetch(`${publicRuntimeConfig.BASE_URL}/api/hello`)
+      .then(response => response.json())
+      .then(setPosts)
+  }, [])
+
+  return (
+    <div>
+      <h1>Spa</h1>
+      {posts &&
+      <div>
+        { posts.map(({ id, name }) => <p key={id}>{id}:{name}</p>)}
+      </div>
+      }
+    </div>)
 }
 
 export default Spa
