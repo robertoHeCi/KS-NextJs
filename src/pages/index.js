d38286b9532c5576fd67f68149c0f0a07919627e
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-01-17 14:10:57 +0100

    feat: add meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,7 @@ export default function Routing () {
   return (
     <div className={layoutStyles.container}>
       <Head>
+        <meta name="description"> NextJS app with routing and data fetching examples</meta>
         <title>Create Next App</title>
         <link rel="icon" href="../../public/favicon.ico" />
       </Head>
