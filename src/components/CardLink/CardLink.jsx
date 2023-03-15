5df7e930fe99e77f8770c9400884f69640438809
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2023-03-15 21:00:33 +0100

    fix: upgrade to Next13

--- src/components/CardLink/CardLink.jsx
+++ src/components/CardLink/CardLink.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
 import cardStyles from '../../styles/Card.module.css'
+import PropTypes from 'prop-types'
 
-export const CardLink = ({ title = '', route = '/', description = '' }) => {
+const CardLink = ({ title = '', route = '/', description = '' }) => {
   return (
-    <Link key={route} href={route}>
-      <a className={cardStyles.card}>
-        <h3>{title} &rarr;</h3>
-        <p>{description}</p>
-      </a>
+    <Link key={route} href={route} className={cardStyles.card}>
+      <h3>{title} &rarr;</h3>
+      <p>{description}</p>
     </Link>
   )
 }
+
+CardLink.propTypes = {
+  title: PropTypes.string,
+  route: PropTypes.string,
+  description: PropTypes.string
+}
+
+export default CardLink
